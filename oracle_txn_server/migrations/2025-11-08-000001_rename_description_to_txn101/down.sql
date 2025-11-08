50f4d2c760ff47bb3a2d1a13c0062dea15d4ecf8
Sergejs Lepins <sergejs.lepins@d8corp.com>
2025-11-08 12:44:18 +0200

    Add migration to rename description column to TXN101
    - Created migration 2025-11-08-000001_rename_description_to_txn101
    - up.sql: ALTER TABLE transactions RENAME COLUMN description TO TXN101
    - down.sql: ALTER TABLE transactions RENAME COLUMN TXN101 TO description
    
    Successfully applied and tested

---
+++ oracle_txn_server/migrations/2025-11-08-000001_rename_description_to_txn101/down.sql
@@ -0,0 +1,2 @@
+-- Rename TXN101 column back to description
+ALTER TABLE transactions RENAME COLUMN TXN101 TO description;
---
+++ oracle_txn_server/migrations/2025-11-08-000001_rename_description_to_txn101/up.sql
@@ -0,0 +1,2 @@
+-- Rename description column to TXN101
+ALTER TABLE transactions RENAME COLUMN description TO TXN101;
